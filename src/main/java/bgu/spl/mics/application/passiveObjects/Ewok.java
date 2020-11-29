57fbdb8965173d392bd779d7c4d6035b80d70bd7
Aner Zakovitch <anerisgreat@gmail.com>
2020-11-29 16:13:04 +0200

    Fixed Ewoks to use correct serial numbers.

--- src/main/java/bgu/spl/mics/application/passiveObjects/Ewok.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Ewok.java
@@ -7,14 +7,11 @@ package bgu.spl.mics.application.passiveObjects;
  * You may add fields and methods to this class as you see fit (including public methods).
  */
 public class Ewok {
-    static int totalSerial = 0;
-
 	int serialNumber;
 	boolean available;
 
     public Ewok(int serialNumber){
-        this.serialNumber = totalSerial;
-        totalSerial++;
+        this.serialNumber = serialNumber;
 
         this.available = true;
     }
