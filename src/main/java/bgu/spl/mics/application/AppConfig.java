0f55da59a648ec046b687fbedd94e3d47b87d34a
Aner Zakovitch <anerisgreat@gmail.com>
2020-12-13 20:55:10 +0200

    Doesn't work, but Main is fleshed out. AppConfig was added for proper loading. Will need to use similar class for output. When run on default json, gets stuck. Need to find out why.

---
+++ src/main/java/bgu/spl/mics/application/AppConfig.java
@@ -0,0 +1,10 @@
+package bgu.spl.mics.application;
+
+import bgu.spl.mics.application.passiveObjects.Attack;
+
+public class AppConfig{
+    public Attack[] attacks;
+    public long R2D2;
+    public long Lando;
+    public int Ewoks;
+}
--- src/main/java/bgu/spl/mics/application/Main.java
+++ src/main/java/bgu/spl/mics/application/Main.java
@@ -1,11 +1,43 @@
 package bgu.spl.mics.application;
 
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import com.google.gson.Gson;
+
+import bgu.spl.mics.application.AppConfig;
+
+import bgu.spl.mics.application.passiveObjects.Diary;
+import bgu.spl.mics.application.passiveObjects.Ewoks;
+import bgu.spl.mics.application.services.*;
+
+
+
 /** This is the Main class of the application. You should parse the input file, 
  * create the different components of the application, and run the system.
  * In the end, you should output a JSON.
  */
 public class Main {
 	public static void main(String[] args) {
-		
+        Gson gson = new Gson();
+        AppConfig config = null;
+        try{
+            FileReader fReader = new FileReader(args[0]);
+            config = gson.fromJson(fReader, AppConfig.class);
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Bad input file!");
+        }
+        if(config != null){
+            //Setting up proper number of Ewoks
+            Ewoks.setNumEwoks(config.Ewoks);
+            
+            Diary d = new Diary();
+
+            new Thread(new C3POMicroservice(d)).run();
+            new Thread(new HanSoloMicroservice(d)).run();
+            new Thread(new LandoMicroservice(config.R2D2, d)).run();
+            new Thread(new LeiaMicroservice(config.attacks, d)).run();
+            new Thread(new R2D2Microservice(config.R2D2, d)).run();
+        }
 	}
 }
