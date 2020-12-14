f7a39ae7696fd0a6dfae11bb6b1ced3dcf709a8e
Aviehu <aviehu@gmail.com>
2020-12-14 15:40:09 +0200

    ready for tests

--- src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Diary.java
@@ -43,37 +43,72 @@ public class Diary {
     public void setTotalAttacks() {
         totalAttacks.addAndGet(1);
     }
-
+    public AtomicInteger getTotalAttacks() { return totalAttacks;}
     public void setHanSoloFinish(long HanSoloFinish) {
         this.HanSoloFinish = HanSoloFinish;
+    }
+
+    public long getHanSoloFinish() {
+        return HanSoloFinish;
     }
 
     public void setC3POFinish(long C3POFinish) {
         this.C3POFinish = C3POFinish;
     }
 
+    public long getC3POFinish() {
+        return C3POFinish;
+    }
+
     public void setC3POTerminate(long C3POTerminate) {
         this.C3POTerminate = C3POTerminate;
+    }
+
+    public long getC3POTerminate() {
+        return C3POTerminate;
     }
 
     public void setHanSoloTerminate(long hanSoloTerminate) {
         HanSoloTerminate = hanSoloTerminate;
     }
 
+    public long getHanSoloTerminate() {
+        return HanSoloTerminate;
+    }
+
     public void setLandoTerminate(long landoTerminate) {
         LandoTerminate = landoTerminate;
+    }
+
+    public long getLandoTerminate() {
+        return LandoTerminate;
     }
 
     public void setLeiaTerminate(long leiaTerminate) {
         LeiaTerminate = leiaTerminate;
     }
 
+    public long getLeiaTerminate() {
+        return LeiaTerminate;
+    }
+
     public void setR2D2Deactivate(long r2D2Deactivate) {
         R2D2Deactivate = r2D2Deactivate;
+    }
+
+    public long getR2D2Deactivate() {
+        return R2D2Deactivate;
     }
 
     public void setR2D2Terminate(long r2D2Terminate) {
         R2D2Terminate = r2D2Terminate;
     }
-    
+
+    public long getR2D2Terminate() {
+        return R2D2Terminate;
+    }
+
+    public void resetNumberAttacks() {
+        totalAttacks.set(0);
+    }
 }
